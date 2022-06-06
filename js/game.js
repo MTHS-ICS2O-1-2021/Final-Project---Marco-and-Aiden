d384117d8adfa2beff8b40b84cb6f94e308ce7eb
Patrick Coxall <mr.coxall@mths.ca>
2022-06-06 14:29:14 -0400

    linter fix

--- js/game.js
+++ js/game.js
@@ -1,22 +1,18 @@
 // Created by: marco cuconato
-// Created on: April 2022    
+// Created on: April 2022
 
 /**
-* Start Phaser
-*/
+ * Start Phaser
+ */
 const config = {
- type: "froggo".AUTO,
+  type: "froggo".AUTO,
   width: 1920,
   height: 1080,
-  // set backgroung color
-  backgroundColor: 0x5f6e7a
-    }
-  },
   // Set BG colour
   backgroundColor: 0xffffff,
   // Set screen in the middle of page
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
