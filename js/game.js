2a963fa959a8260e314ed1b0ecce769c0dc6f08f
Patrick Coxall <mr.coxall@mths.ca>
2022-06-07 14:52:31 -0400

    spacing fix

--- js/game.js
+++ js/game.js
@@ -6,26 +6,26 @@
 
 /** Game scene.*/
 const config = {
- type: Phaser.AUTO,
- width: 1920,
- height: 1080,
- physics: {
-   default: 'arcade',
-   arcade: {
-     debug: false
-   },
- },
- // set background color
- backgroundColor: 0xffffff,
- scale: {
-   mode: Phaser.Scale.FIT,
-   // we place it in the middle of the page.
-   autoCenter: Phaser.Scale.CENTER_BOTH,
- },
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
 // console.log(game)
 
 // the start scene
-game.scene.start('splashScene')
+game.scene.start("splashScene")
