96a11cd581ec002e4be3a126c056d2d76563d8bd
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-06-10 17:05:41 +0000

    add code

--- js/game.js
+++ js/game.js
@@ -1,40 +1,49 @@
 /* global Phaser */
 
 // Created by: marco cuconato
-// Created on: April 2022
-// This is the Phaser3 game configuration file
+// Created on: June 2022
+// This is the final project configuration file
 
-import SplashScene from './splashScene.js'
+// scene import statements
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
 
-// Our game scenes
+// create the new scenes
 const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
 
-/** Game scene.*/
+//* Game scene */
 const config = {
- type: Phaser.AUTO,
- width: 1920,
- height: 1080,
- physics: {
-   default: 'arcade',
-   arcade: {
-     debug: false
-   },
- },
- // set background color
- backgroundColor: 0xffffff,
- scale: {
-   mode: Phaser.Scale.FIT,
-   // we place it in the middle of the page.
-   autoCenter: Phaser.Scale.CENTER_BOTH,
- },
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
-// console.log(game)
 
 // load scenes
-// Note: remember any 'key' is global and CAN NOT be reused!
-game.scene.add('splashScene', splashScene)
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
 
 // the start scene
-game.scene.start('splashScene')
+game.scene.start("splashScene")
--- js/titleScene.js
+++ js/titleScene.js
@@ -1,40 +1,45 @@
-/* global Phaser */
-
-// Created By: marco cuconato
+// Created by: marco cuconato
 // Created on: April 2022
 // This is the Title Scene
 
+/**
+ * This class is the Title Scene.
+ */
 class TitleScene extends Phaser.Scene {
- constructor() {
-   super({ key: 'titleScene' })
+  constructor() {
+    super({ key: "titleScene" })
 
-   this.titleSceneBackgroundImage = null
-   this.titleSceneText = null
-   this.titleSceneTextStyle = { font: '200px Times', fill: 'fde4b9', align: 'center',}
- }
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    }
+  }
 
- init(data) {
-   this.cameras.main.setBackgroundColor('#ffffff')
- }
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff")
+  }
 
- preload() {
-   console.log('Title Scene')
-   this.load.image('titleSceneBackground', 'assets/frog-front.png')
- }
+  preload() {
+    console.log("Title Scene")
+    this.load.image("titleSceneBackground", "./assets/Spearious.svg")
+  }
 
- create(data) {
-   this.titleSceneBackgroundImage = this.add.sprite(0, 0, 'titleSceneBackground').setScale(2.75)
-   this.titleSceneBackgroundImage.x = 1920 / 2
-   this.titleSceneBackgroundImage.y = 1080 / 2
+  create(data) {
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(1)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
+  }
 
-   this.titleSceneText = this.add.text(1920 / 2, 1080 / 2 + 350, 'Froggo', this.titleSceneTextStyle).setOrigin(0.5)
- }
-
- update(time, delta) {
-   if (time > 6000) {
-     this.scene.switch('menuScene')
-   }
- }
+  update(time, delta) {
+    if (time > 6000) {
+      this.scene.switch("menuScene")
+    }
+  }
 }
 
 export default TitleScene
